37b415bc2fd7305eb9dc36a8f628880404807601
milbrathdrew <milbrathdrew@gmail.com>
2024-02-04 14:14:56 -0700

    this is the current working version google auth is broken due to lack of https fixed app.js * route user to /home * user can click Go to Login from /home * user can go back from /login to /home

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <Link to="/login">
+        <button>Go to Login</button>
+      </Link>
+    </div>
+  );
+}
+
+export default Home;
